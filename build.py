e8945b015dd56c8cf3f529605daeafeee05154ba
Fall Xavier <84975017+Fall-Xavier@users.noreply.github.com>
2023-05-08 14:00:30 +0700

    Update build.py

--- build.py
+++ build.py
@@ -2,7 +2,7 @@ import os,sys,shutil
 from distutils.core import setup, Extension
 from Cython.Build import cythonize
 
-main_file = "facebook.cpp"
+main_file = "zero.cpp"
 try:
 	nama = main_file.split(".")[0]
 	setup(
